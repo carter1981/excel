f7e8435149a9306394e51f5a900fa3fa6076e738
carter1981 <rmisjuk@gmail.com>
2020-05-18 14:47:19 +0300

    Finish base framework functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './scss/index.scss';
-console.log('working');
+import {Excel} from './components/excel/Excel';
+import {Header} from './components/header/Header';
+import {Toolbar} from './components/toolbar/Toolbar';
+import {Table} from './components/table/Table';
+import {Formula} from './components/formula/Formula';
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+});
+
+excel.render();
+
